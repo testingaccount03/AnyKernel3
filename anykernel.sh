7f3d8e39bba26e763de87f2c2e39b4576596d690
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-07-07 09:11:09 +0530

    AK3: A6020 edition

--- anykernel.sh
+++ anykernel.sh
@@ -10,12 +10,12 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=ginkgo
-device.name2=willow
+device.name1=A6020
+device.name2=
 device.name3=
 device.name4=
 device.name5=
-supported.versions=10.0
+supported.versions=10.0,9.0
 supported.patchlevels=
 '; } # end properties
 
@@ -24,20 +24,16 @@ block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
-
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
-
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
-
 ## AnyKernel install
 dump_boot;
 write_boot;
 ## end install
-
